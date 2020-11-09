36b79f32a89db2ea2dd4d4d280d7db1b728d1e44
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-11-09 14:28:05 +0100

    Use python logging in Toyota utils

--- src/hero_bringup/toyota_software_utils.py
+++ src/hero_bringup/toyota_software_utils.py
@@ -1,22 +1,22 @@
+import logging
 import os
 import rospkg.common
 import rospkg.rospack
 from xml.etree.ElementTree import ElementTree
 import yaml
 
-global RP
 RP = None
 
 
 def read_dict_yaml(file_path, log):
-    # type: (str, callable) -> dict
+    # type: (str, logging.Logger) -> dict
     """
     Read a yaml file, which should be a dict at toplevel
 
     :param file_path: yaml file to read
     :type file_path: str
-    :param log: Log function to use, must accept a string
-    :type log: callable
+    :param log: Logger to use
+    :type log: logging.Logger
     :return: yaml object of dictionaries and lists
     :rtype: dict
     :raises yaml.parser.ParserError, yaml.scanner.ScannerError: In case of invalid yaml syntax
@@ -28,7 +28,7 @@ def read_dict_yaml(file_path, log):
         except AttributeError:
             yaml_obj = yaml.load(f, yaml.SafeLoader)
         except (yaml.parser.ParserError, yaml.scanner.ScannerError) as e:
-            log("Invalid yaml syntax: {0}".format(e))
+            log.error("Invalid yaml syntax: {0}".format(e))
             raise
 
     return yaml_obj
@@ -80,15 +80,15 @@ def get_installed_toyota_pkgs():
 
 
 def pkgs_to_version_dict(pkg_list, log):
-    # type: (list, callable) -> dict
+    # type: (list, logging.Logger) -> dict
     """
     Get the version of all packages in a list. The results are stored in a dict. The key is the package name and the
     version is stored as the value.
 
     :param pkg_list: List of package names
     :type pkg_list: list
-    :param log: Log function to use, must accept a string
-    :type log: callable
+    :param log: Logger to use
+    :type log: logging.Logger
     :return: Version dictionary
     :rtype: dict
     :raises Exception: In case a package file is missing or can't be parsed
@@ -102,7 +102,7 @@ def pkgs_to_version_dict(pkg_list, log):
             root = ElementTree(None, package_manifest)
             version = root.findtext("version")
         except Exception as e:
-            log("Error during parsing of '{}' of package '{}': {}".format(rospkg.common.PACKAGE_FILE, pkg, e))
+            log.error("Error during parsing of '{}' of package '{}': {}".format(rospkg.common.PACKAGE_FILE, pkg, e))
             raise
         installed_pkgs.update({pkg: version})
 
